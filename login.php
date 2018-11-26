bf8f6f822d3921ce6a1d139125fe9d41539e867d
Ikhlaashul Mu'aasyiqiin <39987415+evnyx@users.noreply.github.com>
2018-11-26 21:53:45 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,88 @@
+<?php
+    session_start();
+    
+    require 'functions.php';
+
+    if(isset($_COOKIE['id'])&& isset($_COOKIE['username']))
+    {
+        $id=$_COOKIE['id'];
+        $key=$_COOKIE['key'];
+
+        $result=mysqli_query($conn,"SELECT username FROM user WHERE id=$id");
+        $row=mysqli_fetch_assoc($result);
+
+        if($key === hash('sha256',$row['username']))
+        {
+            $_SESSION['login']=true;
+        }
+    }
+
+    if(isset($_POST["login"]))
+    {
+        $username=$_POST["username"];
+        $password=$_POST["password"];
+
+
+        $result=mysqli_query($conn,"SELECT * FROM users WHERE username='$username'");
+
+        if(mysqli_num_rows($result)===1)
+        {
+            $row=mysqli_fetch_assoc($result);
+
+            if(password_verify($password,$row["password"]))
+            {
+                $_SESSION["login"]=true;
+                
+                if(isset($_POST['remenber']))
+                {
+                    setcookie('id',$row['id'],time()+60);
+                    setcookie('key',hash(sha256,$row['username']),time()+60);
+                }
+
+                header("Location:index.php");
+                exit;
+            }
+        }
+        $error=true;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Login</title>
+</head>
+<body>
+    <h1> Halaman Login</h1>
+
+    <?php if(isset($error)):?>
+        <p style="color:red; font-style=bold">
+        Username dan password salah</p>
+    
+    <?php endif?>
+
+    
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">username</label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">password</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">Remember Me</label>
+            </li>
+            <li>
+                <button type="submit" name="login">Login</button>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
---
+++ tambah_data.php
@@ -0,0 +1,40 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION["login"]))
+    {
+        echo $_SESSION["login"];
+        header("Location:login.php");
+        exit;
+    }
+
+    require 'tambah.php';
+
+
+    if(isset($_POST['submit']))
+    {
+        var_dump($_POST);
+        var_dump($_FILES);
+        die();
+
+
+        if(tambah($_POST)>0)
+        {
+           echo "
+            <script>
+                alert('data berhasil disimpan');
+                document.location.href='index.php';
+            </script>
+
+            ";
+        }else{
+            echo "
+            <script>
+                alert('data gagal disimpan');
+                document.location.href='tambah_data.php';
+            </script>";
+            echo "<br>";
+            echo mysqli_error($conn);
+        }
+    }
+?>
